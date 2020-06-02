982a13a17aacede074d4fb3123242c973de58c07
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-02 11:46:01 -0400

    made port dynamic in http.js

--- http.js
+++ http.js
@@ -2,8 +2,9 @@ const express = require('express');
 const db = require('./db');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(db.getRandom);
 app.get('/', (req, res) => {});
 
-app.listen(3000);
+app.listen(port);
